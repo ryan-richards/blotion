f1b24c07190cbeb5923a54687676923a3856a349
ryan <ryan.richards@phasora.net>
2022-08-31 13:42:48 +0100

    add starter email endpoint

---
+++ app/routes/api/email/new-signup.tsx
@@ -0,0 +1,23 @@
+import { ActionFunction, json } from "@remix-run/node";
+
+export const action: ActionFunction = async ({ request }) => {
+
+  const data = await request.json();
+
+  //data.record.email
+  var postmark = require("postmark");
+
+  // Send an email:
+  var client = new postmark.ServerClient(process.env.POSTMARK_SERVER_TOKEN);
+
+  client.sendEmail({
+    "From": "hello@blotion.com",
+    "To": "ryan@blotion.com",
+    "Subject": "Get started with Blotion",
+    "HtmlBody": "<strong>Hello</strong> user, thanks for signing up for Blotion. We're excited to help you get started with your new account. <a href='https://guide.blotion.com/'>Click here</a> to get started.",
+    "TextBody": "Hello from Postmark!",
+    "MessageStream": "outbound"
+  });
+
+  return json({ status: 'error' });
+};
