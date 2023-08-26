7e0584f8f8ace1d1d0161d1069ab6d3b7543d314
Ryan Richards <ryan.richards@dawsonadnrews.com>
2023-08-26 08:31:23 +0100

    add resend for welcome emails

--- app/routes/api/email/new-signup.tsx
+++ app/routes/api/email/new-signup.tsx
@@ -1,27 +1,22 @@
 import { ActionFunction, json } from "@remix-run/node";
+import { Resend } from 'resend';
 
 export const action: ActionFunction = async ({ request }) => {
-  const data = await request.json();
-  //data.record.email
-  var postmark = require("postmark");
+    const data = await request.json();
 
-  // Send an email:
-  var client = new postmark.ServerClient(process.env.POSTMARK_SERVER_TOKEN);
+    const resend = new Resend(process.env.RESEND_API_KEY);
 
-  if (!data.record.email) {
-    return json({ status: "false" });
-  }
+    if (!data.record.email) {
+        return json({ status: "false" });
+    }
 
-  await client.sendEmail({
-    From: "hello@blotion.com",
-    To: data.record.email,
-    Subject: "Welcome to Blotion",
-    Tag: "welcome",
-    HtmlBody:
-      "<h1>Welcome</h1><p>Thanks for signing up for Blotion. We're excited to help you get started with your new account.</p><p><a href='https://guide.blotion.com/'>Click here</a> to find lots of help articles you might find useful when setting up your account.</p><p>The easiest way to get started is by following the <a href='https://guide.blotion.com/quick-start'>Quick Start Guide</a></p><p>Any problems, please get in touch!</p><div><span>Many thanks</span><p>Ryan</p></div>",
-    TextBody: "Thanks for signing up for Blotion!",
-    MessageStream: "outbound",
-  });
+    await resend.emails.send({
+        from: "Ryan <hello@blotion.com>",
+        to: [data.record.email],
+        subject: "Welcome to Blotion",
+        html: "<h1>Welcome</h1><p>Thanks for signing up for Blotion. We're excited to help you get started with your new account.</p><p><a href='https://guide.blotion.com/'>Click here</a> to find lots of help articles you might find useful when setting up your account.</p><p>The easiest way to get started is by following the <a href='https://guide.blotion.com/quick-start'>Quick Start Guide</a></p><p>Any problems, please get in touch!</p><div><span>Many thanks</span><p>Ryan</p></div>",
+        text: "Thanks for signing up for Blotion!",
+    });
 
-  return json({ status: "success" });
+    return json({ status: "success" });
 };
