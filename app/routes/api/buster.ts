4301ae306b0319f811eef4d1cac67dbdcd82c063
Ryan Richards <65893895+ryan-richards@users.noreply.github.com>
2025-08-02 01:09:11 +0100

    Update buster.ts

--- app/routes/api/buster.ts
+++ app/routes/api/buster.ts
@@ -1,13 +1,13 @@
 import { ActionFunction } from "@remix-run/node";
-import refreshBlogServer from "~/lib/server/refreshBlog.server";
+//import refreshBlogServer from "~/lib/server/refreshBlog.server";
 
 export const action: ActionFunction = async ({ request }) => {
   const url = new URL(request.url);
   const site = url.searchParams.get("site")?.toString();
 
-  await refreshBlogServer.enqueue(site, {
+  /* await refreshBlogServer.enqueue(site, {
     id: site || "bust-blog",
     delay: "30s",
-  });
+  }); */
   return { status: 200 };
 };
