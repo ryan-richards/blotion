a8b1b2d19ce5cf693fcdcd4ae32f8c60090043cb
Ryan Richards <65893895+ryan-richards@users.noreply.github.com>
2025-08-02 01:07:23 +0100

    Update build-blog.ts

--- app/routes/api/build-blog.ts
+++ app/routes/api/build-blog.ts
@@ -1,12 +1,12 @@
 import { ActionFunction } from "@remix-run/node";
-import buildBlogServer from "~/lib/server/buildBlog.server";
+// import buildBlogServer from "~/lib/server/buildBlog.server";
 
 export const action: ActionFunction = async ({ request }) => {
   const url = new URL(request.url);
   const userId = url.searchParams.get("userId");
 
-  await buildBlogServer.enqueue(url, {
+  /* await buildBlogServer.enqueue(url, {
     id: userId || "build-blog",
-  });
+  });*/ 
   return { status: 200 };
 };
